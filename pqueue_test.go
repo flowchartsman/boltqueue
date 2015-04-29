f2fcd5dc024b6a8c4254f4f5c01dc9a3b0ed3224
Andy Walker <walkeraj@gmail.com>
2015-04-29 13:43:23 -0400

    upqueue.go uses different buckets, but there can be up to 256 of them

--- pqueue_test.go
+++ pqueue_test.go
@@ -7,6 +7,34 @@ import (
 	"time"
 )
 
+func BenchmarkUPQueue(b *testing.B) {
+	queueFile := fmt.Sprintf("%d_test.db", time.Now().UnixNano())
+	queue, err := NewUPQueue(queueFile)
+	if err != nil {
+		b.Error(err)
+	}
+	for n := 0; n < b.N; n++ {
+		for p := 1; p <= 6; p++ {
+			m, _ := NewUMessage(p, fmt.Sprintf("test message %d-%d", p, n))
+			queue.Enqueue(m)
+		}
+	}
+
+	for n := 0; n < b.N; n++ {
+		for p := 1; p <= 6; p++ {
+			_, err := queue.Dequeue()
+			if err != nil {
+				b.Error(err)
+			}
+		}
+	}
+	err = queue.Close()
+	os.Remove(queueFile)
+	if err != nil {
+		b.Error(err)
+	}
+}
+
 func BenchmarkPQueue(b *testing.B) {
 	queueFile := fmt.Sprintf("%d_test.db", time.Now().UnixNano())
 	queue, err := NewPQueue(queueFile)
